af4609566a37df7d14281aff0c721a79f504bce0
Paul McManus <pmcmanus2021@gmail.com>
2020-02-04 17:28:40 +0000

    Passes rollercoaster, height,age restrictions

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -11,12 +11,18 @@ public class RollercoasterTest {
     RollerCoaster rollerCoaster;
     Visitor visitor1;
     Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
+    Visitor visitor5;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
         visitor1 = new Visitor(20,150.00, 20.00);
         visitor2 = new Visitor(20,210.00, 20.00);
+        visitor3 = new Visitor(20,140.00, 20.00);
+        visitor4 = new Visitor(10,210.00, 20.00);
+        visitor5 = new Visitor(20,210.00, 20.00);
     }
 
     @Test
@@ -44,5 +50,21 @@ public class RollercoasterTest {
     public void payDoublePrice(){
         double result = rollerCoaster.priceFor(visitor2);
         assertEquals(16.80, result, 0.1);
+    }
+
+    @Test
+    public void tooShort(){
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
+    }
+
+    @Test
+    public void tooYoung(){
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor4));
+    }
+
+    @Test
+    public void isTallEnoughAndOldEnough(){
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor5));
+
     }
 }
