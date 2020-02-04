5b3629aaf9550337c92ba43e013fea306ba7e52c
Paul McManus <pmcmanus2021@gmail.com>
2020-02-04 20:49:30 +0000

    Adds getAllReview Method, untested

---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,14 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import org.junit.Before;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+    ArrayList<IReviewed> attractions;
+    ArrayList<IReviewed> stalls;
+    
+}
