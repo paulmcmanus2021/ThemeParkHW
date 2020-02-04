dc2979330b8e86785a875f1a144b975c605705da
Paul McManus <pmcmanus2021@gmail.com>
2020-02-04 16:59:16 +0000

    Adds Rollercoaster Height Restriction test

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(20,150.00, 20.00);
+        visitor2 = new Visitor(20,210.00, 20.00);
     }
 
     @Test
@@ -27,5 +32,17 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void payFullPrice(){
+        double result = rollerCoaster.priceFor(visitor1);
+        assertEquals(8.40, result, 0.1);
+    }
+
+    @Test
+    public void payDoublePrice(){
+        double result = rollerCoaster.priceFor(visitor2);
+        assertEquals(16.80, result, 0.1);
     }
 }
